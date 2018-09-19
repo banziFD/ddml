4a327316d62008dcab79b6cd5c6dcd6ce4610bca
banziFD <supengyumail@gmail.com>
2018-09-19 12:50:03 -0400

    change msrcvdata

--- util/data/msrcv.py
+++ util/data/msrcv.py
@@ -3,50 +3,53 @@ import numpy as np
 from PIL import Image
 import glob
 import torch
-import torch
 import torchvision.transforms as transforms
+import json
+import random
 
 def msrcvData(datasetPath, select):
-    image = np.zeros([len(select) * 30, 224, 224, 3], dtype = np.uint8)
-    label = np.zeros(len(select) * 30, dtype = np.uint8)
-    idx = 0
+    folders =['airplane', 'bicycle', 'building', 'car', 'cow', 'face', 'tree']
     l = 0
-    for name in select:
-        filename = glob.glob(datasetPath + '/{}/*s.bmp'.format(name))
-        for f in filename:
+    meta = dict()
+    label = list()
+    idx = 0
+    for folder in folders:
+        files = glob(datasetPath + '/' + folder + '/*.bmp')
+        meta[l] = folder
+        for f in files:
             img = Image.open(f)
-            img = img.resize([224, 224])
-            img = np.array(img.getdata(), dtype = np.uint8)
-            img = img.reshape(224, 224, 3)
-            image[idx] = img
-            label[idx] = l
+            img.save('{}.jpg'.format(idx))
+            label.append(l)
             idx += 1
         l += 1
-    return label, image, label, image
+        test = np.random.choice(len(label), len(label) // 10, replace = False).tolist()
+    train = list()
+    for i in range(len(label)):
+        if i not in test:
+            train.append(i)
+    random.shuffle(train)
+    json.dump(test, open(datasetPath + 'test.json', 'w'))
+    json.dump(train, open(datasetPath + 'train.json', 'w'))
+    json.dump(meta, open(datasetPath + 'meta.json', 'w'))
 
 def prepareData(datasetPath, workPath, select = ['airplane', 'bicycle', 'building', 'car', 'cow', 'face', 'tree']):
     data = msrcvData(datasetPath, select)
-    
-    torch.save(data[0], workPath + '/trainLabel')
-    torch.save(data[1], workPath + '/trainImage')
-    torch.save(data[2], workPath + '/testLabel')
-    torch.save(data[3], workPath + '/testImage')
-    torch.save(data[2], workPath + '/valLabel')
-    torch.save(data[3], workPath + '/valImage')
 
 class MsrcvSet(torch.utils.data.Dataset):
     def __init__(self, workPath, mode = 'train', vecLabel = False, nbClass = 7):
         super(MsrcvSet, self).__init__()
         self.mode = mode
-        self.image = torch.load(workPath + '/{}Image'.format(mode))
-        self.label = torch.load(workPath + '/{}Label'.format(mode))
-        self.transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])])
+        self.image = workPath + '/msrcv'
+        self.label = json.load(workPath + '/mscrv7/label')
+        self.key = json.load(workPath + '/{}'.format(self.mode))
+        self.transform = transforms.Compose([transforms.resize(), transforms.ToTensor(), transforms.Normalize(mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])])
         self.vecLabel = vecLabel
 
     def __getitem__(self, index):
-        x = self.image[index]
+        k = self.key[index]
+        x = Image.open(self.image + '/{}.jpg'.format(k))
         x = self.transform(x)
-        y = self.label[index]
+        y = self.label[k]
         if(self.vecLabel):
             t = torch.zeros(10)
             t[y] = 1
